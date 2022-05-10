ee040ee4836abaa1a180b9a8947818d341104d8e
yeubenham <105216557+yeubenham@users.noreply.github.com>
2022-05-10 19:49:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,18 @@
 const CONFIG = {
     titleWeb: "Yêu bạn Ánh",
     introTitle: 'Vũ Diệp Anh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introDesc: `Xin chào người con gái em yêu đang ở trước màn hình nha :33`,
+    btnIntro: 'Ấn zô đây đy:>',
     title: 'Bé oi 🥰',
     desc: 'Bé ew em hông zợ :> ',
     btnYes: 'Có nha <33',
     btnNo: 'Hông nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu em nhở <3',
     btnReply: 'Gửi cho baby <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay call nha bé.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Bé yêu em đúng khum <33333333',
+    mess: 'Em biết mò 🥰. Yêu bé vãi ò 😘😘',
+    messDesc: 'Bé ôn thi tốt nha .',
+    btnAccept: 'Hết òi:>',
     messLink: '  https://www.facebook.com/profile.php?id=100040997793892' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 
 
